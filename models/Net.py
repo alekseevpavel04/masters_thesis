77a90649b56b02d3f2b9ccdb50d39c58c48f8dc5
alekseevpavel04 <alekceevpave@mail.ru>
2024-10-29 14:26:54 +0300

    fix

--- models/Net.py
+++ models/Net.py
@@ -1,101 +1,52 @@
 import torch
-from torch import nn as nn
-from torch.nn import functional as F
-from torch.nn import init
-import math
+import torch.nn as nn
+import torch.nn.functional as F
 
-def make_layer(basic_block, num_basic_block, **kwarg):
-    """Make layers by stacking the same blocks."""
-    layers = []
-    for _ in range(num_basic_block):
-        layers.append(basic_block(**kwarg))
-    return nn.Sequential(*layers)
-
-class ResidualDenseBlock_5C(nn.Module):
-    def __init__(self, nf=64, gc=32, bias=True):
-        super(ResidualDenseBlock_5C, self).__init__()
-        # gc: growth channel, i.e. intermediate channels
-        self.conv1 = nn.Conv2d(nf, gc, 3, 1, 1, bias=bias)
-        self.conv2 = nn.Conv2d(nf + gc, gc, 3, 1, 1, bias=bias)
-        self.conv3 = nn.Conv2d(nf + 2 * gc, gc, 3, 1, 1, bias=bias)
-        self.conv4 = nn.Conv2d(nf + 3 * gc, gc, 3, 1, 1, bias=bias)
-        self.conv5 = nn.Conv2d(nf + 4 * gc, nf, 3, 1, 1, bias=bias)
-        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+class RDBLayer(nn.Module):
+    def __init__(self, num_feat=64):
+        super(RDBLayer, self).__init__()
+        self.conv1 = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
+        self.conv2 = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
+        self.conv3 = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
+        self.conv4 = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
+        self.conv5 = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
 
     def forward(self, x):
-        x1 = self.lrelu(self.conv1(x))
-        x2 = self.lrelu(self.conv2(torch.cat((x, x1), 1)))
-        x3 = self.lrelu(self.conv3(torch.cat((x, x1, x2), 1)))
-        x4 = self.lrelu(self.conv4(torch.cat((x, x1, x2, x3), 1)))
-        x5 = self.conv5(torch.cat((x, x1, x2, x3, x4), 1))
-        return x5 * 0.2 + x
-
-class RRDB(nn.Module):
-    '''Residual in Residual Dense Block'''
-    def __init__(self, nf, gc=32):
-        super(RRDB, self).__init__()
-        self.RDB1 = ResidualDenseBlock_5C(nf, gc)
-        self.RDB2 = ResidualDenseBlock_5C(nf, gc)
-        self.RDB3 = ResidualDenseBlock_5C(nf, gc)
-
-    def forward(self, x):
-        out = self.RDB1(x)
-        out = self.RDB2(out)
-        out = self.RDB3(out)
-        return out * 0.2 + x
+        out1 = F.leaky_relu(self.conv1(x), negative_slope=0.2, inplace=True)
+        out2 = F.leaky_relu(self.conv2(out1), negative_slope=0.2, inplace=True)
+        out3 = F.leaky_relu(self.conv3(out2), negative_slope=0.2, inplace=True)
+        out4 = F.leaky_relu(self.conv4(out3), negative_slope=0.2, inplace=True)
+        out5 = self.conv5(out4)
+        return out5 + x
 
 class RRDBNet(nn.Module):
-    def __init__(self, in_nc=3, out_nc=3, nf=64, nb=23, gc=32, scale=4):
+    def __init__(self, num_in_ch=3, num_out_ch=3, num_feat=64):
         super(RRDBNet, self).__init__()
-        self.scale = scale
         
-        if scale == 2:
-            in_nc = in_nc * 4
-        elif scale == 1:
-            in_nc = in_nc * 16
-            
-        self.conv_first = nn.Conv2d(in_nc, nf, 3, 1, 1, bias=True)
-        self.RRDB_trunk = make_layer(RRDB, nb, nf=nf, gc=gc)
-        self.trunk_conv = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+        # первый слой
+        self.conv_first = nn.Conv2d(num_in_ch, num_feat, 3, 1, 1)
         
-        # Upsampling
-        self.upconv1 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
-        self.upconv2 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
-        self.HRconv = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
-        self.conv_last = nn.Conv2d(nf, out_nc, 3, 1, 1, bias=True)
+        # основная часть сети
+        self.body = nn.ModuleList()
+        # судя по ключам, у нас только один блок в body (body.0)
+        rdb_block = nn.ModuleDict({
+            'rdb1': RDBLayer(num_feat),
+            'rdb2': RDBLayer(num_feat),
+            'rdb3': RDBLayer(num_feat)
+        })
+        self.body.append(rdb_block)
 
-        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        # последний слой
+        self.conv_last = nn.Conv2d(num_feat, num_out_ch, 3, 1, 1)
 
     def forward(self, x):
-        if self.scale == 2:
-            feat = pixel_unshuffle(x, scale=2)
-        elif self.scale == 1:
-            feat = pixel_unshuffle(x, scale=4)
-        else:
-            feat = x
-            
-        fea = self.conv_first(feat)
-        trunk = self.trunk_conv(self.RRDB_trunk(fea))
-        fea = fea + trunk
-
-        fea = self.lrelu(self.upconv1(F.interpolate(fea, scale_factor=2, mode='nearest')))
-        fea = self.lrelu(self.upconv2(F.interpolate(fea, scale_factor=2, mode='nearest')))
-        out = self.conv_last(self.lrelu(self.HRconv(fea)))
-
+        feat = self.conv_first(x)
+        
+        body_feat = feat
+        for block in self.body:
+            body_feat = block['rdb1'](body_feat)
+            body_feat = block['rdb2'](body_feat)
+            body_feat = block['rdb3'](body_feat)
+        
+        out = self.conv_last(body_feat)
         return out
-
-def pixel_unshuffle(x, scale):
-    """ Pixel unshuffle.
-    Args:
-        x (Tensor): Input feature with shape (b, c, hh, hw).
-        scale (int): Downsample ratio.
-    Returns:
-        Tensor: the pixel unshuffled feature.
-    """
-    b, c, hh, hw = x.size()
-    out_channel = c * (scale**2)
-    assert hh % scale == 0 and hw % scale == 0
-    h = hh // scale
-    w = hw // scale
-    x_view = x.view(b, c, h, scale, w, scale)
-    return x_view.permute(0, 1, 3, 5, 2, 4).reshape(b, out_channel, h, w)
